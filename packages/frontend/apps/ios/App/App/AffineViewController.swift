7386cfe499691bea679be97223f60d25c1b6e1a8
砍砍 <git@qaq.wiki>
2024-11-18 22:06:39 +0800

    WIP: 打个框架先

--- packages/frontend/apps/ios/App/App/AffineViewController.swift
+++ packages/frontend/apps/ios/App/App/AffineViewController.swift
@@ -1,17 +1,30 @@
 import UIKit
 import Capacitor
+import Intelligents
 
 class AFFiNEViewController: CAPBridgeViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
     // disable by default, enable manually when there is a "back" button in page-header
     webView?.allowsBackForwardNavigationGestures = false
+    self.navigationController?.navigationBar.isHidden = true
+    installIntelligentsButton()
   }
   
   override func capacitorDidLoad() {
     bridge?.registerPluginInstance(CookiePlugin())
     bridge?.registerPluginInstance(HashcashPlugin())
     bridge?.registerPluginInstance(NavigationGesturePlugin())
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: animated)
+    self.presentIntelligentsButton()
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    dismissIntelligentsButton()
   }
 }
