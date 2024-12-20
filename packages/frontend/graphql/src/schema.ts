6a547840683cd9af55cc1a840cc093b98f13ad22
DarkSky <darksky2048@gmail.com>
2024-12-20 15:33:21 +0800

    chore: cleanup codes

--- packages/frontend/graphql/src/schema.ts
+++ packages/frontend/graphql/src/schema.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+ 
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = {
@@ -285,6 +285,7 @@ export type ErrorDataUnion =
   | SubscriptionNotExistsDataType
   | SubscriptionPlanNotFoundDataType
   | UnknownOauthProviderDataType
+  | UnsupportedClientVersionDataType
   | UnsupportedSubscriptionPlanDataType
   | VersionRejectedDataType
   | WrongSignInCredentialsDataType;
@@ -361,6 +362,7 @@ export enum ErrorNames {
   TOO_MANY_REQUEST = 'TOO_MANY_REQUEST',
   UNKNOWN_OAUTH_PROVIDER = 'UNKNOWN_OAUTH_PROVIDER',
   UNSPLASH_IS_NOT_CONFIGURED = 'UNSPLASH_IS_NOT_CONFIGURED',
+  UNSUPPORTED_CLIENT_VERSION = 'UNSUPPORTED_CLIENT_VERSION',
   UNSUPPORTED_SUBSCRIPTION_PLAN = 'UNSUPPORTED_SUBSCRIPTION_PLAN',
   USER_AVATAR_NOT_FOUND = 'USER_AVATAR_NOT_FOUND',
   USER_NOT_FOUND = 'USER_NOT_FOUND',
@@ -1206,6 +1208,13 @@ export enum SubscriptionVariant {
 export interface UnknownOauthProviderDataType {
   __typename?: 'UnknownOauthProviderDataType';
   name: Scalars['String']['output'];
+}
+
+export interface UnsupportedClientVersionDataType {
+  __typename?: 'UnsupportedClientVersionDataType';
+  action: Scalars['String']['output'];
+  clientVersion: Scalars['String']['output'];
+  recommendedVersion: Scalars['String']['output'];
 }
 
 export interface UnsupportedSubscriptionPlanDataType {
